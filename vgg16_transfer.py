ea2654d449c573acea6d9abcdda57068251361ea
hogehoge <kusafusa.k@gmail.com>
2019-12-15 16:47:04 +0900

    ちょっと修正

--- vgg16_transfer.py
+++ vgg16_transfer.py
@@ -4,21 +4,23 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import SGD, Adam
-from tensorflow.python.keras.utils import to_categorical
+from tensorflow.python.keras.utils import np_utils
 from tensorflow.python.keras.applications import VGG16
 
-classes = ['car', 'motorbike']
+# パラメーターの初期化
+classes = ["car", "motorbike"]
 num_classes = len(classes)
 image_size = 224
 
-X_train, X_test, y_train, y_test = np.load("./imagefiles224.npy")
-y_train = to_categorical(y_train, num_classes)
-y_test = to_categorical(y_test, num_classes)
-X_train = X_train.astype('float') / 255.0
-X_test = X_test.astype('float') / 255.0
+# データの読み込み
+X_train, X_test, y_train, y_test = np.load("./imagefiles_224.npy",allow_pickle=True)
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
+X_train = X_train.astype("float") / 255.0
+X_test = X_test.astype("float") /255.0
 
-model = VGG16(weights='imagenet', include_top=False,
-              input_shape=(image_size, image_size, 3))
+# モデルの定義
+model = VGG16(weights='imagenet', include_top=False, input_shape=(image_size,image_size,3))
 # print('Model loaded')
 # model.summary()
 
@@ -28,22 +30,18 @@ top_model.add(Dense(256, activation='relu'))
 top_model.add(Dropout(0.5))
 top_model.add(Dense(num_classes, activation='softmax'))
 
-# モデルの結合
 model = Model(inputs=model.input, outputs=top_model(model.output))
-# model.summary()
 
+# model.summary()
 for layer in model.layers[:15]:
     layer.trainable = False
 
-# opt = SGD(lr=0.01)
-opt = Adam(lr=0.0001)
-model.compile(loss='categorical_crossentropy',
-              optimize=opt, metrics=['accuracy'])
 
+opt = Adam(lr=0.0001)
+model.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
 model.fit(X_train, y_train, batch_size=32, epochs=17)
+
 score = model.evaluate(X_test, y_test, batch_size=32)
 
-model.save('./vgg16_transfer.h5')
+model.save("./vgg16_transfer.h5")
 
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
